b1a659bcb327ce232dd56e89e4c8698335691868
naufaldi <naufaldi.rafif@gmail.com>
2022-01-20 09:39:57 +0700

    add init

---
+++ weeks1/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+ arrowParens: 'always',
+ singleQuote: true,
+ jsxSingleQuote: true,
+ tabWidth: 2,
+ semi: true,
+ printWidth: 300,
+ tabWidth: 2,
+ trailingComma: 'all',
+ useTabs: false,
+};
